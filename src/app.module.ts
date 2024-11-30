7925d80e17631ce6e5d2ad9ffc2e6039ada6df46
Mahi_G13 <maicolgallego14@gmail.com>
2024-11-29 20:03:49 -0500

    chore: connection with the postgres database
    - perform correct connection with the postgres database
    - general configurations: cors, validation pipe, Class Serializer Interceptor and swagger for documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize:
+          configService.get<string>('STAGE') === 'prod' ? false : true, // Automatically synchronize the database schema (development only)
+        logging: false, // Enable logging of SQL queries (development only)
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1/');
+
+  //enable CORS (Cross-Origin Resource Sharing)
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  //enable deserialization of response objects (apply class-transformer)
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  //Config Swagger to API Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Budget management API')
+    .setDescription(`An API meant for customers to manage their numbers!`)
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('budgets-management/api/v1/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
