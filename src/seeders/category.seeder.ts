d342fc2033dc9c9cd90229efdfe9a0cbaa2f579e
Mahi_G13 <maicolgallego14@gmail.com>
2024-12-02 12:12:29 -0500

    refactor(categories): optimize findAll method and improve query performance
    - Replaced multiple value checks with `IN` query using TypeORM to optimize performance.
    - Added user ID parameter to ensure users can only access their own data.
    - Implemented basic error handling with try-catch blocks to capture errors.
    - Removed unnecessary endpoints for creating categories and searching by name in the controller.

--- src/seeders/category.seeder.ts
+++ src/seeders/category.seeder.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from 'src/categories/entities/category.entity';
-import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class CategorySeeder {
